1a9cfff74d1b704ca49ac48f535c4073c61cd838
Harry Sun <ssun2@Harrys-MacBook-Pro.local>
2023-08-23 20:58:42 -0500

    add quicker patching pipeline

--- DeepHeme/mask_and_patch.py
+++ DeepHeme/mask_and_patch.py
@@ -16,12 +16,15 @@ from tqdm import tqdm
 from multiprocessing import Pool  
 from functools import partial 
 
+from concurrent.futures import ThreadPoolExecutor 
+from concurrent.futures import ThreadPoolExecutor, as_completed  
+
+### import self built packages
+from quality_check import get_top_view_preselection_mask
+from utils.image_basics import convert_4_to_3
+
 ### ray is a library for parallel computing
 ### Neo use this!
-
-
-### this is a function that leverage the low resolution mask to guide patching the high resolution image
-
 
 class patching_based_on_mask():
     def __init__(self, low_res_mask, high_res_image_ndpi, patch_size, overlap_threshold, savepath, slidename):
@@ -39,27 +42,33 @@ class patching_based_on_mask():
         self.savepath = savepath
         self.slidename = slidename
 
-    def save_patch(self, args):
-        _x, _y = args
-        patching = self.high_res_image_ndpi.read_region((int(_x/self.patch_size), int(_y/self.patch_size)), 0, (self.patch_size, self.patch_size))
-        patching = cv2.cvtColor(np.array(patching), cv2.COLOR_RGB2BGR)
-        cv2.imwrite(f"{self.savepath}/{self.slidename.split('.ndpi')[0]}patches/patch_{str(_x)}_{str(_y)}.png", patching)
-
-
-
     #### this is a function that patch the high resolution image based on the low resolution mask
-    def patch_high_res_image(self, save = False, number_of_cpu_cores = 1):
+    def patch_high_res_image(self, save = False, num_processes = 1):
+        
         ### get the shape of the high resolution image
         ### the image is readed by openslide,
         ### so the shape is (height, width, channel)
         high_res_image_shape  = self.high_res_image_ndpi.level_dimensions[0]
-        ### reshape the low resolution mask to the same shape as the high resolution image
-        low_res_mask = cv2.resize(self.low_res_mask, (high_res_image_shape[1], high_res_image_shape[0]))
         
-        # change the mask from (255,255,255) to (1)
-        low_res_mask = low_res_mask[:,:,0]
-        low_res_mask[low_res_mask>0] = 1
+        ### reshape the low resolution mask to the same shape as the high resolution image
+        #low_res_mask = cv2.resize(self.low_res_mask, (high_res_image_shape[1], high_res_image_shape[0]))
+        
+        
+        ### get the largest dimension of the openslide object
+        WSI_shape = self.high_res_image_ndpi.level_dimensions[0]
+        
 
+        # change the mask from (255,255,255) to (1)
+        #assert 1==2, 'stop here for now'
+        low_res_mask = self.low_res_mask
+
+        if len(low_res_mask) == 3:
+            low_res_mask = low_res_mask[:,:,0]
+        
+        low_res_mask[low_res_mask>0] = 1
+        
+        
+        
         ### checking the overlap between the patch and the mask
         ### if the overlap is larger than the threshold, then the patch is valid
         ### otherwise, the patch is invalid
@@ -72,23 +81,51 @@ class patching_based_on_mask():
         ### initialize the patch mask
         patch_mask = np.zeros((num_patch_height, num_patch_width))
 
-        ### compute the overlap between the patch and the mask
-  
-        # initialize the patch mask  
-        patch_mask = np.zeros((num_patch_height, num_patch_width))  
         
+
+        ### compute the overlap between the patch and the mask
+        ratio = int(WSI_shape[0]/low_res_mask.shape[1])
+        
+
+   
         # compute the overlap between the patch and the mask using a sliding window approach  
         stride = self.patch_size  
-        window_shape = (self.patch_size, self.patch_size)  
-        num_slices = tuple((sz - ws + 1) // stride for sz, ws in zip(low_res_mask.shape, window_shape))  
-        strided_low_res_mask = np.lib.stride_tricks.sliding_window_view(low_res_mask, window_shape)  
-        
-        # calculate the sum over each patch  
-        patch_sums = np.sum(strided_low_res_mask, axis=(-1, -2))  
-        
-        # create a mask where the overlap is larger than the threshold  
-        patch_mask = (patch_sums / (self.patch_size * self.patch_size)) > self.overlap_threshold
+        window_shape = (int(self.patch_size/ratio), int(self.patch_size/ratio))
 
+        ### iteration through the low resolution mask
+        ### if the non-zero value is more than the thresdhold, then the patch is valid
+        ### otherwise, the patch is invalid, that woule be represented in the patch_mask by zero and one
+        ### zero means invalid, one means valid
+        for i in tqdm(range(0, low_res_mask.shape[0] - window_shape[0] + 1, window_shape[0])):
+            for j in range(0, low_res_mask.shape[1] - window_shape[1] + 1, window_shape[1]):
+                ### get the patch
+                
+                patch = low_res_mask[i:i+window_shape[0], j:j+window_shape[1]]
+                
+                ### check the dimension of the patch
+                ### get the number of non-zero elements
+                num_non_zero = np.count_nonzero(patch)
+                
+                ### if the number of non-zero elements is more than the threshold, then the patch is valid
+                if num_non_zero > window_shape[0]*window_shape[1]*self.overlap_threshold:
+                    ### set the value of the patch mask to be 1
+                    patch_mask[int(j/window_shape[0]), int(i/window_shape[1])] = 1
+        ### 
+        
+
+         
+    
+        ### show the patch mask
+        ### convert the patch_mask to uint8
+        #patch_mask_viz = (patch_mask*255).astype(np.uint8)
+
+        #plt.imshow(patch_mask_viz)
+        #plt.show()
+
+        #assert 1==2, 'stop here for now'
+
+        # the x and y coordinates of the patch are flipped, so I transpose the patch_mask
+        patch_mask = patch_mask.T
         ### get the valid patch index
         # get the valid patch index in the height direction
         # get the valid patch index in the width direction
@@ -102,22 +139,64 @@ class patching_based_on_mask():
         ### get the valid patch
         # initialize the valid patch
         # Create a list of arguments (_x, _y) for valid patches  
-        valid_patches = list(zip(valid_patch_index_height, valid_patch_index_width))  
-        if save:  
-            # Set the number of processes (use the number of CPU cores for optimal performance)  
-            num_processes = os.cpu_count()  
-            # warning if the number of processes is larger than the  maximum number of cpu cores
-            if num_processes > number_of_cpu_cores:
-                print(f"Warning: the number of processes {num_processes} is larger than the maximum number of cpu cores {number_of_cpu_cores}")
-                ### set the number of processes to the maximum number of cpu cores
-                num_processes = number_of_cpu_cores
-                print(f"Set the number of processes to {num_processes}")
+        valid_patches = list(zip(valid_patch_index_height*512, valid_patch_index_width*512)) 
 
+        return valid_patches 
+# main function
+if __name__ == '__main__':
+    ### use openslide to read the high resolution image
+    high_res_image_ndpi = openslide.OpenSlide('/Users/ssun2/Documents/DeepHeme/H17-6116_S10_MSK6_2023-04-27_18.30.44.ndpi')
+    ### get the levels of openslide object
+    _level = high_res_image_ndpi.level_count - 1
+    ### get the lowest resolution of the high resolution image
+    low_res_image = high_res_image_ndpi.read_region((0,0), _level, high_res_image_ndpi.level_dimensions[-1])
+
+    ### show the low resolution image
+    show_flag = False
+    if show_flag:
+        plt.imshow(low_res_image)
+        plt.show()
+    low_res_image = convert_4_to_3(np.array(low_res_image))
+
+    #plt.imshow(low_res_image)
+        
+    print('The shape of the low resolution image is {}'.format(low_res_image.shape))
+
+    mask = get_top_view_preselection_mask(low_res_image, RGB=False, verbose=False)
+
+    # display the mask
+    print('The shape of the mask is {}'.format(mask.shape))
+
+    patching_machine = patching_based_on_mask(low_res_mask=mask, 
+                                              high_res_image_ndpi=high_res_image_ndpi, 
+                                              patch_size=512, 
+                                              overlap_threshold=0.5, 
+                                              savepath='/Users/ssun2/Documents/DeepHeme/patches', 
+                                              slidename='H17-6116_S10_MSK6_2023-04-27_18.30.44.ndpi')
+    
+    #patching_machine.patch_high_res_image(save=True,num_processes=1)
+
+
+
+    valid_patches = patching_machine.patch_high_res_image(save=True,num_processes=1)
+
+    def save_patch( _x, _y):
+        patch_size = patching_machine.patch_size
                 
-            # Create a Pool and execute the save_patch function in parallel  
-            with Pool(processes=num_processes) as pool:  
-                func = partial()
-                pool.map(func, valid_patches)  
+
+        ### get the patch
+        patching = patching_machine.high_res_image_ndpi.read_region((_x, _y), 0, (patch_size, patch_size))
+        patching = cv2.cvtColor(np.array(patching), cv2.COLOR_RGB2BGR)
+        cv2.imwrite(f"{patching_machine.savepath}/{patching_machine.slidename.split('.ndpi')[0]}patches/patch_{str(_x)}_{str(_y)}.png", patching)
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(save_patch, x, y) for x, y in valid_patches] 
+        for future in tqdm(as_completed(futures), total=len(valid_patches), desc="Processing patches"):  
+                pass  # You can retrieve the result here if needed using future.result()  
+    
+
+
+    
 
 
 
