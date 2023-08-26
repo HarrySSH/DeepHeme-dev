d0d945336f47ac323fa8b79497f452f8327d9ce1
Harry Sun <ssun2@Harrys-MacBook-Pro.local>
2023-08-26 08:49:48 -0500

    minor changes

--- DeepHeme/DeepHeme_implementation.py
+++ DeepHeme/DeepHeme_implementation.py
@@ -14,15 +14,40 @@ class DeepHeme():
     ### This is the command line: !python Cell_classifier.py --patch_repo_dir /media/hdd3/harry/Slides_repo/Plasma_cel_myeloma/patches/H18-2459_S10_MSKC_2023-05-31_16.29.27patches 
     ### Please call the function and perform the analysis
     ### In the time also inform the user the progress of the analysis as well as the total time cost
-    def __init__(self, registered = False, patch_repo_dir = None):
+    def __init__(self, registered = False,):
         self.registered = registered
-        self.patch_repo_dir = patch_repo_dir
+        self.wsi_dir = None
+        self.wsi_name = None
+        self.patch_repo_dir = None
+        #self.patch_repo_dir = patch_repo_dir
+    
+    def set_wsi_dir(self, wsi_dir, wsi_name):
+        ### This is the function to set the wsi directory
+        self.wsi_dir = wsi_dir
+        assert os.path.exists(self.wsi_dir), "The wsi directory does not exist"
+        self.wsi_name = wsi_name
+        assert os.path.exists(os.path.join(self.wsi_dir, self.wsi_name)), "The wsi does not exist"
+        ### created the overall patch foler format from /media/hdd4/harry/Slides_repo/AML/slides/ -> /media/hdd4/harry/Slides_repo/AML/patches/
+        if not os.path.exists(self.wsi_dir.split('slides')[0] + 'patches'):
+            os.mkdir(self.wsi_dir.split('slides')[0] + 'patches')
+
+        self.patch_repo_dir = self.wsi_dir.split('slides')[0] + 'patches' + self.wsi_name.split('.ndpi')[0] + 'patches'
+        if not os.path.exists(self.patch_repo_dir):
+            os.mkdir(self.patch_repo_dir)
+        self.registered = True
     
     def set_patch_repo_dir(self, patch_repo_dir):
         ### This is the function to set the patch repo directory
         self.patch_repo_dir = patch_repo_dir
         assert os.path.exists(self.patch_repo_dir), "The patch repo directory does not exist"
         self.registered = True
+
+    def patching(self):
+        ### this is the function to perforn patching in the multi-processing way
+        start_time = time.time()
+        print("Start patching")
+        
+        os.system("python patch_and_mask.py --wsi_dir {} --wsi_name {} ".format(self.wsi_dir, self.wsi_name))
     
     def patch_classifier(self):
         ### This is the function to call the patch classifier
@@ -51,6 +76,8 @@ class DeepHeme():
         print("Cell classification finished")
         print("Time cost: {} seconds".format(time.time() - start_time))
 
+    
+
     def ready(self):
         ### check if the registration is done
         return self.registered
@@ -58,6 +85,7 @@ class DeepHeme():
     def run(self):
         assert self.ready(), "Please register the patch repo directory first"
         ### This is the function to run the whole analysis
+        self.patching()
         self.patch_classifier()
         self.cell_detector()
         self.cell_classifier()
